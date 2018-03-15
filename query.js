48d47ab6e8b8fc5ba371bfcf62ce66eb64075edf
wartkeskaraman <vartkess_123@hotmail.com>
2018-03-15 18:16:13 +0200

    Add files via upload

---
+++ query.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $("#qu").mouseover(function(){
+        $("#qu").css("color", "#2223A4");
+    });
+    $("#qu").mouseout(function(){
+        $("#qu").css("color", "black");
+    });
+});
+
+$(document).ready(function(){
+    $("#qu2").mouseover(function(){
+        $("#qu2").css("color", "#2223A4");
+    });
+    $("#qu2").mouseout(function(){
+        $("#qu2").css("color", "black");
+    });
+});
+
+$(document).ready(function(){
+    $("#travel").mouseover(function(){
+        $("#travel").css("color", "#9DA8C7");
+    });
+    $("#travel").mouseout(function(){
+        $("#travel").css("color", "#EBEBEB");
+    });
+});
